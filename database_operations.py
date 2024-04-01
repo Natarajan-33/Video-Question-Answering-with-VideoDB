6b2514fc18cfd99fb8ed4708bd9a949fe4ae32bb
Natarajan Mohankumar <natarajan.mohan33@gmail.com>
2024-04-01 10:42:26 +0530

    Optimized collection object

--- database_operations.py
+++ database_operations.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from videodb import connect, SearchType, play_stream
 
@@ -7,64 +8,93 @@ load_dotenv()
 # Establish a connection to the video database
 videodb_api_key = os.getenv("VIDEODB_KEY")
 connection = connect(api_key=videodb_api_key)
-collection = connection.get_collection()
+# collection = connection.get_collection()
 
-def add_videos_to_index(youtube_urls):
+def add_videos_to_index(collection_name, youtube_urls):
+    video_dict = {}
+    collection = connection.create_collection(name =collection_name, description = collection_name)
     """Uploads videos to the database and indexes their spoken words."""
     for url in youtube_urls:
         try:
-            collection.upload(url=url)
-            print(f"Video: {url} uploaded successfully.")
-            for video in collection.get_videos():
-                video.index_spoken_words()
-                print(f"Indexed spoken words in: {video.name}.")
+            video = collection.upload(url=url)
+            video_dict[video.name] = video.id
+            st.write(f"Video: {video.name}({url}) uploaded successfully.")
+            
         except Exception as error:
-            print(f"Failed to upload and index {url}. Error: {error}")
+            st.write(f"Failed to upload and index {url}. Error: {error}")
+            return None , None
+        
+    for video in collection.get_videos():
+        video.index_spoken_words()
+        st.write(f"Indexed spoken words in: {video.name}.")
+        
+    return video_dict, collection
 
-def find_related_content_by_query(query):
-    """Searches the database for content similar to the query and returns the first result."""
+# def find_related_content_by_query(query):
+#     """Searches the database for content similar to the query and returns the first result."""
+#     try:
+#         search_results = collection.search(query=query)
+#         first_result = search_results.get_shots()[0]
+#         return first_result.text, {"video_title": first_result.video_title, "text": first_result.text}
+#     except Exception as error:
+#         print(f"Search failed for query '{query}'. Error: {error}")
+#         return "", {}
+    
+
+def chat_with_video(collection, video_id, query):
     try:
-        search_results = collection.search(query=query)
+        video = collection.get_video(video_id)
+        search_results = video.search(query=query)
         first_result = search_results.get_shots()[0]
         return first_result.text, {"video_title": first_result.video_title, "text": first_result.text}
     except Exception as error:
         print(f"Search failed for query '{query}'. Error: {error}")
         return "", {}
-    
 
-def stream_video(video_url):
-    video = connection.upload(url=video_url)
+
+def stream_video(collection, video_id):
+    # video = connection.upload(url=video_id)
+    video = collection.get_video(video_id)
     video.generate_stream()
     video.play()
 
-def watch_shorts(video_url, topic):
-    video = connection.upload(url=video_url)
-    video.index_spoken_words()
+def watch_shorts(collection, video_id, topic):
+    video = collection.get_video(video_id)
+    # video.index_spoken_words()
     result = video.search(query=topic)
     return result
 
-def transcribe_video(video_url):
-    video = connection.upload(url=video_url)
+def transcribe_video(collection, video_id):
+    video = collection.get_video(video_id)
     # get text of the spoken content
     # text_json = video.get_transcript()
     text = video.get_transcript_text()
     return text
 
-def add_subtitles(video_url):
-    video = connection.upload(url=video_url)
-    video.index_spoken_words()
+def add_subtitles(collection, video_id):
+    video = collection.get_video(video_id)
+    # video.index_spoken_words()
     new_stream = video.add_subtitle()
     play_stream(new_stream)
 
-def thumbnail(video_url):
-    video = connection.upload(url=video_url)
+def thumbnail(collection, video_id):
+    video = collection.get_video(video_id)
     return video.generate_thumbnail()
 
-def delete_video_from_index(video_url):
-    video = connection.upload(url=video_url)
+def delete_video_from_index(collection, video_id):
+    video = collection.get_video(video_id)
     video.delete()
 
-def delete_all_videos_from_index():
-    coll = connection.get_collection()
-    for video in coll.get_videos():
+def delete_all_videos_from_index(collection):
+    # coll = connection.get_collection()
+    for video in collection.get_videos():
         video.delete()
+
+
+def show_collection(collection):
+    videos_list = []
+    # coll = connection.get_collection()
+    for video in collection.get_videos():
+        videos_list.append(video)
+    return videos_list
+
--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from database_operations import add_videos_to_index, find_related_content_by_query, stream_video, watch_shorts, transcribe_video, add_subtitles, thumbnail, delete_video_from_index, delete_all_videos_from_index
+from database_operations import add_videos_to_index, chat_with_video, stream_video, watch_shorts, transcribe_video, add_subtitles, thumbnail, delete_video_from_index, delete_all_videos_from_index, show_collection
 from advanced_language_model import generate_answer_from_context
 
 
@@ -7,24 +7,42 @@ st.set_page_config(
     page_title="Video Insight Bot🤖", layout="wide", initial_sidebar_state="auto"
 )
 
-def setup_session_variables():
-    if "chat_history" not in st.session_state:
-        st.session_state.chat_history = [
-            {"role": "bot", "message": "Hello! Feel free to search through the video content. What's your question?"}
-        ]
+st.sidebar.title("Video Insight Bot🤖")
+st.sidebar.divider()
 
+
+
+
+
+def setup_session_variables():
+    # if "chat_history" not in st.session_state:
+    #     st.session_state.chat_history = [
+    #         {"role": "bot", "message": "Hello! Feel free to search through the video content. What's your question?"}
+    #     ]
     if "video_urls" not in st.session_state:
         st.session_state.video_urls = []
-
     if "urls_stored" not in st.session_state:
         st.session_state.urls_stored = False
     if "first_time" not in st.session_state:
         st.session_state.first_time = True
+    if "collection" not in st.session_state:
+        st.session_state.collection = None
+    if "collection_variables" not in st.session_state:
+        st.session_state.collection_variables = False
+    if "video_dict" not in st.session_state:
+        st.session_state.video_dict = {}
+    if 'chat_histories' not in st.session_state:
+        st.session_state.chat_histories = {}  # Stores chat histories for each video
+
+
+
 
 setup_session_variables()
 
 if not st.session_state.urls_stored:
-    st.header("Provide the YouTube Video URL")
+    st.header("Provide the collection name for your videos")
+    collection_name = st.text_input("", placeholder="One collection name", label_visibility="collapsed")
+    st.subheader("Provide the YouTube Video URL")
     video_url = st.text_input("", placeholder="Paste here", label_visibility="collapsed")
     if st.button("Add Video URL to Library"):
         st.session_state.video_urls.append(video_url)
@@ -33,10 +51,16 @@ if not st.session_state.urls_stored:
         video_url = ""
 
     if st.button("Save All Video URLs to Database"):
-        st.session_state.urls_stored = True
-        with st.spinner("Uploading Videos and Indexing..."):
-            add_videos_to_index(st.session_state.video_urls)
-            st.success("Videos uploaded and indexed successfully!")
+        if collection_name != "":
+            with st.spinner("Uploading Videos and Indexing..."):
+                st.session_state.video_dict, st.session_state.collection = add_videos_to_index(collection_name, st.session_state.video_urls)
+                if st.session_state.video_dict != None:
+                    st.success("Videos uploaded and indexed successfully!")
+                    st.session_state.urls_stored = True
+                    st.session_state.collection_variables = True
+                else:
+                    st.error("Error uploading videos and indexing. Please try again.")
+                    st.session_state.urls_stored = False
 
 elif st.session_state.first_time:
     st.success("Video URLs have been successfully saved.")
@@ -55,54 +79,61 @@ selected_service = st.sidebar.radio(
 if selected_service == "***LLM Summary***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
-    st.title("Video Insight Bot🤖")
+    # st.title("Video Insight Bot🤖")
 
-    for entry in st.session_state.chat_history:
-        with st.chat_message(entry["role"]):
-            st.write(entry["message"])
+    st.subheader("Select URL to chat with")
+    video_name = st.selectbox(" ", st.session_state.video_dict.keys(), placeholder="Choose the video to stream",index=None, disabled=False, label_visibility="collapsed", key="chatbot")
+    if video_name:
+        if video_name not in st.session_state.chat_histories:
+            st.session_state.chat_histories[video_name] = [{"role": "bot", "message": "Hello! Feel free to search through the video content. What's your question?"}] # Initialize chat history for the video if not exist
+        
+        for entry in st.session_state.chat_histories[video_name]:
+            with st.chat_message(entry["role"]):
+                st.write(entry["message"])
 
-    query = st.chat_input("What would you like to know?")
-    if query:
-        st.session_state.chat_history.append({"role": "user", "message": query})
-        with st.chat_message("user"):
-            st.write(query)
 
-    if st.session_state.chat_history[-1]["role"] != "bot":
-        with st.chat_message("bot"):
-            with st.spinner("Analyzing..."):
-                response_placeholder = st.empty()
-                search_context, details = find_related_content_by_query(query)
-                response = ""
-                response = generate_answer_from_context(query, search_context)
-            response_placeholder.write(response)
+        query = st.chat_input("What would you like to know?")
+        if query:
+            st.session_state.chat_histories[video_name].append({"role": "user", "message": query})
+            with st.chat_message("user"):
+                st.write(query)
 
-            st.session_state.chat_history.append({"role": "bot", "message": response})
+        if st.session_state.chat_histories[video_name][-1]["role"] != "bot":
+            with st.chat_message("bot"):
+                with st.spinner("Analyzing..."):
+                    response_placeholder = st.empty()
+                    search_context, details = chat_with_video(st.session_state.collection, st.session_state.video_dict[video_name], query)
+                    response = ""
+                    response = generate_answer_from_context(query, search_context)
+                response_placeholder.write(response)
 
-        with st.expander("Context and Details"):
-            st.write(details)
+                st.session_state.chat_histories[video_name].append({"role": "bot", "message": response})
+
+            with st.expander("Context and Details"):
+                st.write(details)
 
 
 if selected_service == "***Stream Full Video***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
     st.subheader("Select URL to stream it")
-    video_link = st.selectbox(" ", st.session_state.video_urls, placeholder="Choose the video to stream",index=None, disabled=False, label_visibility="collapsed", key="stream")
-    if video_link:
+    video_name= st.selectbox(" ", st.session_state.video_dict.keys(), placeholder="Choose the video to stream",index=None, disabled=False, label_visibility="collapsed", key="stream")
+    if video_name:
         with st.spinner("Streaming video in new tab..."):
-            stream_video(video_link)
+            stream_video(st.session_state.collection, st.session_state.video_dict[video_name])
 
 
 if selected_service == "***Search and Watch Clip***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
     st.subheader("Select URL to stream a clip from")
-    video_link = st.selectbox(" ", st.session_state.video_urls, placeholder="Choose the video whose shorts you want to watch from", index=None, disabled=False, label_visibility="collapsed", key="shorts")
-    if video_link:
+    video_name = st.selectbox(" ", st.session_state.video_dict.keys(), placeholder="Choose the video whose shorts you want to watch from", index=None, disabled=False, label_visibility="collapsed", key="shorts")
+    if video_name:
         st.subheader("Enter the topic to get shorts relevant to that")
         topic = st.text_input(" ", placeholder="ask here")
         if topic:
             with st.spinner("Streaming shots in new tab..."):
-                response = watch_shorts(video_link, topic)  
+                response = watch_shorts(st.session_state.collection, st.session_state.video_dict[video_name], topic)  
                 if response.shots != []:
                     response.play()
                 else:
@@ -113,11 +144,11 @@ if selected_service == "***Get Transcript***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
     st.subheader("Select URL to get transcript from")
-    video_link = st.selectbox(" ", st.session_state.video_urls, placeholder="Choose an option", index=None, disabled=False, label_visibility="collapsed", key="transcript")
-    if video_link:
+    video_name = st.selectbox(" ", st.session_state.video_dict.keys(), placeholder="Choose an option", index=None, disabled=False, label_visibility="collapsed", key="transcript")
+    if video_name:
         with st.spinner("Transcribing video..."):
-            transcription = transcribe_video(video_link)  
-            st.info(f"Transcript for {video_link} is:")
+            transcription = transcribe_video(st.session_state.collection, st.session_state.video_dict[video_name])  
+            st.info(f"Transcript for {video_name} is:")
             st.success(transcription)
 
 
@@ -125,32 +156,34 @@ if selected_service == "***Add Subtitles***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
     st.subheader("Select URL to add subtitles to")
-    video_link = st.selectbox(" ", st.session_state.video_urls, placeholder="Choose an option", index=None, disabled=False, label_visibility="collapsed", key="subtitles")
-    if video_link:
+    video_name = st.selectbox(" ", st.session_state.video_dict.keys(), placeholder="Choose an option", index=None, disabled=False, label_visibility="collapsed", key="subtitles")
+    if video_name:
         with st.spinner("Adding subtitles to video and streaming video in new tab..."):
-            transcription = add_subtitles(video_link)  
+            transcription = add_subtitles(st.session_state.collection, st.session_state.video_dict[video_name])  
 
 
 if selected_service == "***Generate Thumbnail***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
     st.subheader("Select URL to which you want to generate thumbnail for")
-    video_link = st.selectbox(" ", st.session_state.video_urls, placeholder="Choose an option", index=None, disabled=False, label_visibility="collapsed", key="thumbnail")
-    if video_link:
+    video_name = st.selectbox(" ",st.session_state.video_dict.keys(), placeholder="Choose an option", index=None, disabled=False, label_visibility="collapsed", key="thumbnail")
+    if video_name:
         with st.spinner("Generating thumbnail..."):
-            thumbnail_image = thumbnail(video_link)  
+            thumbnail_image = thumbnail(st.session_state.collection, st.session_state.video_dict[video_name])  
             st.image(thumbnail_image, width=300)
 
 
 if selected_service == "***Delete Video***" and st.session_state.urls_stored:
     st.header(selected_service)
     st.divider()
-    st.subheader("Select URL to which you want to generate thumbnail for")
-    video_link = st.selectbox(" ", st.session_state.video_urls, index=None, placeholder="Choose an option", disabled=False, label_visibility="collapsed", key="delete_video")
-    if video_link:
+    st.subheader("Select URL of the video which you want to delete")
+    video_name = st.selectbox(" ", st.session_state.video_dict.keys(), index=None, placeholder="Choose an option", disabled=False, label_visibility="collapsed", key="delete_video")
+    if video_name:
         with st.spinner("Deleting video..."):
-            delete_video_from_index(video_link)  
-            st.session_state.video_urls.remove(video_link)
+            delete_video_from_index(st.session_state.collection, st.session_state.video_dict[video_name])  
+            # st.session_state.video_urls.remove(video_link)
+            if video_name in st.session_state.video_dict[video_name]:
+                del st.session_state.video_dict[video_name]
             st.success("Video deleted successfully from the index.")
 
 
@@ -159,13 +192,22 @@ if selected_service == "***Delete All***" and st.session_state.urls_stored:
     st.divider()
     if st.button("Click here to Delete All Videos"):
         with st.spinner("Deleting all videos..."):
-            delete_all_videos_from_index()  
+            delete_all_videos_from_index(st.session_state.collection)  
             st.session_state.video_urls = []
+            st.session_state.video_dict.clear()
             st.session_state.urls_stored = False
             st.session_state.chat_history = [
             {"role": "bot", "message": "Hello! Feel free to search through the video content. What's your question?"}
         ]
             st.success("All videos deleted successfully from the index.")
+
+st.sidebar.divider()
+if st.sidebar.button("Check collection") and st.session_state.collection_variables:
+    video_list = show_collection(st.session_state.collection)
+    st.divider()
+    st.write(video_list)
+
+
 
 
 
